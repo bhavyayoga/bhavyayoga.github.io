88881a66b5d98eda80dbd95845fafdd7429cdf5b
AlexFreik <alexfreik5@gmail.com>
2024-05-08 16:16:51 +0530

    fix after rewiw, add dynamic programs fetch

--- src/script.js
+++ src/script.js
@@ -20,10 +20,43 @@ async function fetchScheduleItems() {
     return scheduleItems;
 }
 
+function createProgramCard(info) {
+    const div = document.createElement('div');
+    div.className = 'inline-block';
+    div.innerHTML = `
+      <div class="class-card card">
+        <figure>
+          <img src="/img/${info['Image']}" alt="${info['Name']} Image" />
+        </figure>
+        <div class="card-body p-4">
+          <h2 class="card-title">${info['Name']}</h2>
+          <p>${info['Date']}</p>
+          <p>
+            <a href="${info['Google Maps URL']}" class="link" target="_blank">
+              ${info['Address']}
+            </a>
+          </p>
+          <p>
+            <b>${info['Price']}</b>
+            , incl. online support
+          </p>
+          <div class="card-actions justify-start">
+            <button class="btn btn-accent" onclick="modalApply.showModal()">Apply</button>
+            <a href="#programs-info" class="btn btn-outline btn-accent">Learn More</a>
+          </div>
+        </div>
+      </div>`;
+    return div;
+}
+
 async function updateProgramsDiv() {
     const programsDiv = document.getElementById('programs');
     const items = await fetchScheduleItems();
     console.log(items);
+
+    items.forEach((item) => programsDiv.appendChild(createProgramCard(item)));
+
+    document.getElementById('programs-loading').remove();
 }
 
 updateProgramsDiv();
